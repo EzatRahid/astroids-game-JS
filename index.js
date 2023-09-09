6a7fd1636dbfedda52ad3adff5c43ba497fd58ed
EzatRashid <ezat.b.rashid@gmail.com>
2023-09-08 19:40:14 -0600

    player rotation started

--- index.js
+++ index.js
@@ -12,11 +12,16 @@ canvas.height = window.innerHeight
 
 
 class Player {
-    constructor({position,velocity}){
+    constructor({position,velocity,rotation}){
         this.position = position
         this.velocity = velocity
+        this.rotation = 0
     }
     draw(){
+        c.save();
+        c.translate(this.position.x , this.position.y)
+        c.rotate(this.rotation)
+        c.translate(-this.position.x, -this.position.y)
         // c.fillStyle='green'
         // c.fillRect(this.position.x,this.position.y,100,100)
         c.arc(this.position.x,this.position.y, 5,0,Math.PI * 2,false)
@@ -30,6 +35,7 @@ class Player {
 
         c.strokeStyle = 'white'
         c.stroke();
+        c.restore();
     }
     update(){
         this.draw()
@@ -68,7 +74,13 @@ const animate = () => {
 
     if(keys.w.pressed){
         player.velocity.x = 1
+    }else{
+        player.velocity.x = 0
     }
+    if(keys.d.pressed){
+        player.rotation += 0.01
+    }
+
 }
 
 animate();
@@ -78,16 +90,37 @@ window.addEventListener('keydown',(event) =>{
     
     switch(event.code){
         case 'KeyW':
-        console.log('w key was pressed')
+      
         keys.w.pressed = true
         break
         case 'KeyA':
-        console.log('a key was pressed')
+    
         keys.a.pressed = true
         break
         case 'KeyD':
-        console.log('d key was pressed')
+     
         keys.d.pressed = true
+        break
+
+    }
+
+
+})
+window.addEventListener('keyup',(event) =>{
+    console.log(event)
+    
+    switch(event.code){
+        case 'KeyW':
+      
+        keys.w.pressed = false
+        break
+        case 'KeyA':
+    
+        keys.a.pressed = false
+        break
+        case 'KeyD':
+     
+        keys.d.pressed = false
         break
 
     }
@@ -99,64 +132,3 @@ window.addEventListener('keydown',(event) =>{
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const keys = {
-//     w:{
-//         pressed:false 
-//     },
-    
-//     a:{
-//         pressed:false 
-//     },
-    
-//     d:{
-//         pressed:false 
-//     },
-    
-// }
-
-// const animate = () =>{
-//     window.requestAnimationFrame(animate)
-//     c.fillStyle = 'black'
-//     c.fillRect(0,0,canvas.width,canvas.height)
-//     player.update();
-
-//     if(keys.w.pressed) player.velocity.x = 1
-    
-// }
-// animate();
-
-// window.addEventListener('keydown', (event) =>{
-//     switch(event.code){
-//         case 'KeyW':
-//             console.log('W was pressed')
-//             keys.w.pressed = true
-//             break
-//         case 'KeyA':
-//             console.log('A was pressed')
-//             keys.a.pressed = true
-//             break
-//         case 'KeyD':
-//             console.log('D was pressed')
-//             keys.d.pressed = true
-//             break
-        
-//     }
-//     console.log(event)
-// })
